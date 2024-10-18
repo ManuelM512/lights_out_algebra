4a5cc10a08dd901bdd6003ea001eb32490342845
Manuel Mongelós <110827049+ManuelM512@users.noreply.github.com>
2024-10-17 21:57:29 -0300

    better results

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from array_utils import prettier_result
 from lights_out import lights_out_solver
 from matrix_generator import random_matrix_generator
 
@@ -6,7 +7,12 @@ def main():
     matrix = random_matrix_generator(3)
     print("Matriz a resolver")
     print(matrix)
-    print("Solución:")
-    print(lights_out_solver(matrix))
+    print("\nArray de solución:")
+    solution_array = lights_out_solver(matrix)
+    print(solution_array)
+    print("\nSolución estilizada para ver mejor qué botones apretar:")
+    print(prettier_result(solution_array))
 
-# poner el coso de __main
+
+if __name__ == "__main__":
+    main()
